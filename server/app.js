212cdc8acd12f53326354c8a10991e4e68d655d4
SumitVermakgp <sumitverma9628@gmail.com>
2021-10-25 14:43:02 +0900

    add end points

--- server/app.js
+++ server/app.js
@@ -29,6 +29,82 @@ app.get("/api/notes", async (req, res) => {
   }
 });
 
+// get all note titles
+app.get("/api/notecards", async (req, res) => {
+  try {
+    const notecards = await db.select('title').table("notes");
+    res.json(notecards);
+  } catch (err) {
+    console.error("Error loading notes!", err);
+    res.sendStatus(500);
+  }
+});
+
+
+
+// get note by id
+app.get("/api/note/:id", async (req, res) => {
+  const { id } = req.params;
+  try{
+      const note = await knex.select("*").table('notes').where(`id`, id);
+      if(user) {
+          res.status(200).send(note);
+      } else {
+          res.status(404).send("Records not found")
+      }
+  } catch (error){
+      res.status(500).json({message: "Error geeting the note details", error: error})
+  }
+});
+
+// add a  note
+app.post("/api/note", async (req, res) => {
+  try {
+    const { title, message } = req.body;
+    const note = await knex('notes').insert({'title': title, 'message': message})
+    if(note){
+      res.status(201).send("Note Added")
+    } else {
+      res.status(404).send("Bad Rquest")
+    }
+  } catch (err) {
+    res.status(500).json({message: "Error adding the note", error: error})
+  }
+});
+
+// update a note
+app.patch("/api/note/:id", async (req, res) => {
+  const { id } = req.params;
+  const modifier = req.body;
+
+  try {
+      const update =  await knex('notes').where({id}).update(modifier);
+      if (update){
+          res.status(200).send("Update Successfully!")
+      } else {
+          res.status(404).send("Record not found")
+      } 
+  } catch (error){
+      res.status(500).json({message: "Error updating note", error: error})
+  }
+});
+
+// delete a note
+app.delete("/api/note/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+      const update =  await knex('notes').where({id}).del();
+      if (update){
+          res.status(200).send("Deleted Successfully!")
+      } else {
+          res.status(404).send("Record not found")
+      } 
+  } catch (error){
+      res.status(500).json({message: "Error deleting the note", error: error})
+  }
+});
+
 
 
 // Serve static assets
