c42a4a7a8bd2c29fa6efce325ca5e21d7e81ebaa
SumitVermakgp <sumitverma9628@gmail.com>
2021-10-25 16:55:03 +0900

    fix design

--- src/components/NoteTakingSpace.jsx
+++ src/components/NoteTakingSpace.jsx
@@ -4,9 +4,11 @@ import React from "react";
 export default function NoteTakingSpace(props) {
 	return (
 		<section className="note">
-			<textarea className="noteTitle"> Title Here </textarea> 
+			<textarea className="noteTitle"> Redux Architecture </textarea> 
 			<br></br>
-			<textarea className="noteSummary"> Summary Here</textarea>
+			<textarea className="noteSummary"> The store, action, and reducer are the main building blocks of the Redux architecture. There is a single store for the global state of the app. Actions can only change the states. To specify how a state gets updated in response to an action, we need to write pure reducer functions that calculate a new state based on the old state and the action.
+
+</textarea>
 		</section>
 	);
 };
