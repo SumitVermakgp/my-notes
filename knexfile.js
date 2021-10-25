dbc0593eeb84b86f0df7414700a845f9cfa521c3
SumitVermakgp <sumitverma9628@gmail.com>
2021-10-25 13:31:44 +0900

    fix

--- knexfile.js
+++ knexfile.js
@@ -23,6 +23,22 @@ var options = {
       directory: `${__dirname}/seeds`,
     },
   },
+  
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 
   production: {
     client: "pg",
